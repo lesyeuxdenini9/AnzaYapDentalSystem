d83f9b04e8c606563976ebce2daf77fde8361ef0
Alvin S. Raquem <41246806+asraquem00110@users.noreply.github.com>
2020-09-06 12:18:44 +0800

    Update addModalComponent.vue

--- src/components/patient/reservation/addModalComponent.vue
+++ src/components/patient/reservation/addModalComponent.vue
@@ -271,7 +271,14 @@ export default {
             }else{
                 this.reservation.transaction = this.followups[index].id,
                 this.reservation.dentist = this.followups[index].dentistId,
-                transact.style.background = "silver"
+                 transact.style.background = "silver"
+                this.reservation.servicelist = this.followups[index].Treatments.map((treatment)=>{
+                    return {
+                        id:treatment.serviceId,
+                        service:treatment.service,
+                        price:treatment.amount,
+                    }
+                })
             }
         },
         closethis: function(){
